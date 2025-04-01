6912eb05f0c5e6dc174cf9c17c302317076981e0
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-04-01 19:18:32 +0530

    mods

--- components/websocket-provider.tsx
+++ components/websocket-provider.tsx
@@ -7,21 +7,6 @@ import { useEffect } from "react"
 // This component simulates WebSocket connections and events
 export function WebSocketProvider({ children }: { children: React.ReactNode }) {
 
-  useEffect(() => {
-    const tradeWs = new WebSocket('wss://ws.coincap.io/trades/binance')
-    tradeWs.onmessage = function (msg) {
-      console.log(msg.data)
-
-    }
-    const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin')
-
-    pricesWs.onmessage = function (msg) {
-      console.log(msg.data)
-
-    }
-
-
-  }, [])
 
 
   return <>{children}</>
